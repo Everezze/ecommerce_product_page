a8cbc806179f44d12b25fb5b87719cc61f9c64d9
omarsalim <83906211+Everezze@users.noreply.github.com>
2025-07-01 15:07:24 +0200

    added cart toggle

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,14 @@
+import React from "react"
 import Cart from "./Cart";
 
 export default function Header(props){
+
+	const [showCart,setShowCart] = React.useState(false);
+
+	function toggleCart(){
+		setShowCart(prevState => !prevState);
+	}
+
 	return (
 		<header>
 			<div className="mobile-menu">
@@ -56,9 +64,11 @@ export default function Header(props){
 				</nav>
 			</div>
 			<div className="right-nav">
-				<div className="img-container cart-img">
+				<div className="img-container cart-img" onClick={toggleCart}>
 					<img className="curspoint" src="images/icon-cart.svg" alt="" />
+					{showCart && 
 					<Cart quantity={props.quantity} removeProduct={props.removeProduct}/>
+					}
 				</div>
 				<div className="img-container profile-pic">
 					<img src="images/image-avatar.png" alt="" />
